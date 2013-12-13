8bd3e5ced955b1e0cb03df4cb91db23f2bb3cfcd
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-13 14:26:00 +0900

    プロパティ値設定

--- RC2CryptoSample/RC2CryptoSample/RC2.cs
+++ RC2CryptoSample/RC2CryptoSample/RC2.cs
@@ -8,7 +8,22 @@ namespace RC2CryptoSample
 {
     public class RC2
     {
-        
+
+        //暗号キー
+        private string _CryptoKey;
+
+        private string _NomalMessage;
+
+        private string _EncryptionMessage;
+
+        public RC2(string cryptoKey, string nomalMessage )
+        {
+            this._CryptoKey = cryptoKey;
+            this._NomalMessage = nomalMessage;
+        }
+
+
+     
 
 
     }
