a1cb975e4d0e8fb909b39cfe7a5ed674a406304d
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2013-12-13 14:37:18 +0900

    DES版で一度作成

--- RC2CryptoSample/RC2CryptoSample/RC2.cs
+++ RC2CryptoSample/RC2CryptoSample/RC2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,14 +17,39 @@ namespace RC2CryptoSample
 
         private string _EncryptionMessage;
 
+        private const string IV = "000";
+
+
         public RC2(string cryptoKey, string nomalMessage )
         {
             this._CryptoKey = cryptoKey;
             this._NomalMessage = nomalMessage;
         }
 
+        public string EncryptString()
+        {
+            using (SymmetricAlgorithm csp = new DESCryptoServiceProvider())
+            {
+                csp.Key = getByts(this._CryptoKey);
+                csp.IV = getByts(IV);
 
-     
+                using (ICryptoTransform transform = csp.CreateEncryptor())
+                {
+                    byte[] source = Encoding.UTF8.GetBytes(_NomalMessage);
+                    byte[] encrypted = transform.TransformFinalBlock(source, 0, source.Length);
+
+                    _EncryptionMessage = Convert.ToBase64String(encrypted);
+                }
+
+            }
+        }
+
+
+
+        private byte[] getByts(string txt)
+        {
+            return ASCIIEncoding.ASCII.GetBytes(txt);
+        }
 
 
     }
